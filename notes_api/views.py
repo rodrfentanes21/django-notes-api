771c55d4459b1ed359bda30d526c2b9d01c6e0c7
Rodrigo <rodrigo.fentanes@sofex.com.br>
2023-09-21 15:03:12 -0300

    created views class

--- notes_api/views.py
+++ notes_api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from notes_api import models
+
+
+class ApiViewRoutes(APIView):
+    """App routes that use API View (document what routes belong here later)"""
