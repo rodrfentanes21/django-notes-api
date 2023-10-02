b8160581aba0c1b6324c3273d17776145b3e35db
Rodrigo <rodrigo.fentanes@sofex.com.br>
2023-10-02 13:10:49 -0300

    hotfix

--- notes_project/settings.py
+++ notes_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ijmma6d^nsx65k7!&s_xvz!m+9^8jw6!kkpb$*_zs#e#*y!c04'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
